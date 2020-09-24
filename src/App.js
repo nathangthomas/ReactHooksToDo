31cec45a250aa8082e69acf9101b3ab2a2df77b4
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-09-24 01:03:14 -0500

    Add todo and app funcitons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Todo({ todo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    {text: "Learn React"},
+    {text: "Get a better grasp on React Hooks"},
+    {text: "Dive into Redux"},
+  ]);
+
+  return(
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index)=> (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
